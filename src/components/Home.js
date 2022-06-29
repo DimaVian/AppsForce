c2cc929e28a1a1450a59d3d9385f239766c3eac9
DimaVian <viandima41@gmail.com>
2022-06-29 19:25:19 +0300

    releasev1

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import AddUser from './AddUser'
 import './User.css';
 import axios from 'axios';
 
-function Home(){
+const Home= () =>{
     const[list,setList] = useState([]);
 
     useEffect(()=>{
@@ -14,26 +14,28 @@ function Home(){
             setList(res.data.results)
         })
     },[]);
+    
 
     const userRemove = (userId)=>{
         const newList = [...list];
         const index = list.findIndex((user)=>user.id === userId);
-
         newList.splice(index,1);
         setList(newList);
     }
     
     return(
         <div className='Container'>
+            {console.log(list)}
            <AddUser list={list} setList={setList}/>
-            {list.map(user=>(
-                <div className='User'>
-               <p className='Picture'><img className='PictureBorder' src={user.picture.large}/></p>
+            {list.map((user,i) =>(
+                <div key={i}className='User'>
+               {user.picture ? <p className='Picture'><img className='PictureBorder' src={user.picture.large}/></p>:<p className='Picture'><img className='PictureBorder' src={''}/></p>} 
                <div className='Info'>
-               <p>Name: {user.name.first} {user.name.last}</p>
+                {user.name.first?<p>Name: {user.name.first} {user.name.last}</p>:<p>Name: {user.name}</p> }
+               
                <p>Email: {user.email}</p>
-               <p>Location: {user.location.country} , {user.location.city} , {user.location.street.name}</p>
-               <p>User Id: {user.id.name}</p>
+               {user.location.country?<p>Location: {user.location.country} , {user.location.city} , {user.location.street.name}</p>:<p>Location: {user.location}</p>}
+               {}<p>User Id: {user.id.value}</p>
                <button className='dButton' onClick={()=>userRemove(user.id)}>Delete</button>
                </div>
                </div>
